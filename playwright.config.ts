2999c0476cb04f2af13932a56a6e2b1b487b0505
lmchkhi <146848152+lmchkhi@users.noreply.github.com>
2026-08-10 10:32:52 +0700

    Update config to reduce flaky test

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
         ['html', { open: 'never', title: "Run by 23127381", outputFolder: "test-report/chorme" }], 
@@ -33,7 +34,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
